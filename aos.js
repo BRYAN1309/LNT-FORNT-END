ceee6fad83519273e83c48bd38e35ea0b596fd49
BRYAN1309 <145699542+BRYAN1309@users.noreply.github.com>
2024-02-02 23:49:57 +0700

    Add files via upload

---
+++ aos.js
@@ -0,0 +1,21 @@
+AOS.init();
+
+AOS.init({
+  disable: false, 
+  startEvent: 'DOMContentLoaded', 
+  initClassName: 'aos-init', 
+  animatedClassName: 'aos-animate', 
+  useClassNames: false, 
+  disableMutationObserver: false, 
+  debounceDelay: 50, 
+  throttleDelay: 99, 
+  offset: 120, 
+
+  offset: 50, 
+  delay: 100, 
+  duration: 600,
+  easing: 'ease', 
+  once: true, 
+  mirror: false, 
+  anchorPlacement: 'top-bottom', 
+});
